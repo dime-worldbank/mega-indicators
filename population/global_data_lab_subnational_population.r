267ad3720d6665b8c0ff2b7ea3d4e9c99a8ad6f5
weilu <weilu>
2023-11-14 18:31:40 +0000

    Remove extrapolated population data

--- population/global_data_lab_subnational_population.r
+++ population/global_data_lab_subnational_population.r
@@ -23,6 +23,11 @@ sess <- sess %>%
     set_dataset('areadata') %>%
     set_countries_all() %>%
     set_indicators(c('regpopm'))
+    # by default linear extrapolation for 3 years
+    # disabling extrapolation doesn't seem to work
+    # set_extrapolation_years_linear(0) %>%
+    # set_extrapolation_years_nearest(0) %>%
+    # set_interpolation(TRUE)
 spop_merged <- gdl_request(sess)
 print(colnames(spop_merged))
 print(paste('nrow:', nrow(spop_merged)))
@@ -37,14 +42,23 @@ sparkR.session(appName = "global_data_lab", config = hive_config)
 
 # COMMAND ----------
 
+library(dplyr)
+
 key <- 'X'
 
 df <- gather(spop_merged, key = key, value = 'population_millions', -Country, -Continent, -ISO_Code, -Level, -GDLCODE, -Region) %>%
-  dplyr::mutate(year = as.integer(parse_number(key))) %>%
-  dplyr::select(-key)
+  mutate(year = as.integer(parse_number(key))) %>%
+  select(-key)
 
-sdf <- createDataFrame(df)
+df_no_extrapolation <- df %>%
+  filter(!is.na(population_millions)) %>%  # Drop rows where population_millions is null
+  group_by(Country, Region) %>%
+  arrange(Country, Region, desc(year)) %>%  
+  filter(row_number() > 3) %>%  # Drop extrapolated years (last 3 years given Country, Region)
+  ungroup()
+
+sdf <- createDataFrame(df_no_extrapolation)
 table_name <- paste0("indicator.global_data_lab_subnational_population")
 saveAsTable(sdf, tableName = table_name, mode = "overwrite")
 
-print(paste(table_name, 'nrow:', nrow(df)))
+print(paste(table_name, 'nrow:', nrow(df_no_extrapolation)))
