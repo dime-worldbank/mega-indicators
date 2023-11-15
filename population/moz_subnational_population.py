18fe5eec80f59ba43645d39b7a4757de0a2693fc
weilu <weilu>
2023-11-15 04:31:41 +0000

    Normalize admin level 1 column name
    and replace country code with country name for easy join with existing tables

--- population/moz_subnational_population.py
+++ population/moz_subnational_population.py
@@ -24,22 +24,22 @@ def remove_accents(text):
 
 df = pd.concat([pop_07_16, pop_17_50], ignore_index=True)
 df['region'] = df['region'].apply(remove_accents)
-df.rename(columns={'region': 'adm1_name', 'Date': 'year', 'Value': 'population'}, inplace=True)
+df.rename(columns={'region': 'adm1_name_alt', 'Date': 'year', 'Value': 'population'}, inplace=True)
 
-df['country_code'] = 'MOZ'
+df['country_name'] = 'Mozambique'
 
-columns_ordered = ['country_code', 'adm1_name', 'year', 'population', 'data_source']
+columns_ordered = ['country_name', 'adm1_name_alt', 'year', 'population', 'data_source']
 df = df.reindex(columns=columns_ordered)
 df
 
 # COMMAND ----------
 
 # Check regions
-num_regions = len(df.adm1_name.unique())
+num_regions = len(df.adm1_name_alt.unique())
 assert num_regions == 11, f'Expect there to be 11 regions got {num_regions}'
 
 # Check year coverage for each region
-for region, group in df.groupby('adm1_name')['year']:
+for region, group in df.groupby('adm1_name_alt')['year']:
     assert group.iloc[0] == 2007, f"Invalid start year for region {region}"
     assert group.iloc[-1] == 2050, f"Invalid end year for region {region}"
     assert all(y2 - y1 == 1 for y1, y2 in zip(group, group[1:])), f"Missing years in region {region}"
