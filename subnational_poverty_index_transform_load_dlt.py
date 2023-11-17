a45ff71dcbe2f63b26e64fb7c10ecb02838c865e
weilu <weilu>
2023-11-17 17:34:45 +0000

    Normalize Maputo city name

--- subnational_poverty_index_transform_load_dlt.py
+++ subnational_poverty_index_transform_load_dlt.py
@@ -1,13 +1,13 @@
 # Databricks notebook source
 import dlt
-from pyspark.sql.functions import col, regexp_replace, trim
+from pyspark.sql.functions import col, regexp_replace, trim, when
 
 @dlt.table(name=f'subnational_poverty_index')
 def subnational_poverty_index():
     countries = spark.table(f'indicator.country').select('country_name', 'country_code')
     return (spark.table(f'indicator_intermediate.poverty_index_spid_gsap')
-        .withColumn('region_name_tmp', trim(regexp_replace(col("region_name"), "[-\[\]–]+", " ")))
-        .withColumn('region_name_alt', regexp_replace(col("region_name_tmp"), "[\\d]+", ""))
+        .withColumn('region_name_tmp', regexp_replace(trim(regexp_replace(col("region_name"), "[-\[\]–]+", " ")), "[\\d]+", ""))
+        .withColumn('region_name_alt', when(col("region_name_tmp").isin(['Maputo City', 'Maputo Cidade']), 'Cidade de Maputo').otherwise(col("region_name_tmp")))
         .drop('region_name_tmp')
         .join(countries, ["country_code"], "inner") # TODO: change to left & investigate dropped
     )
