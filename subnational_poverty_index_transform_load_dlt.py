80c59ad6c2156fef01c5b0d28c9d85038a7b27a3
weilu <weilu>
2023-11-17 17:49:59 +0000

    Fix maputo city name and add province normalization

--- subnational_poverty_index_transform_load_dlt.py
+++ subnational_poverty_index_transform_load_dlt.py
@@ -6,8 +6,25 @@ from pyspark.sql.functions import col, regexp_replace, trim, when
 def subnational_poverty_index():
     countries = spark.table(f'indicator.country').select('country_name', 'country_code')
     return (spark.table(f'indicator_intermediate.poverty_index_spid_gsap')
-        .withColumn('region_name_tmp', regexp_replace(trim(regexp_replace(col("region_name"), "[-\[\]–]+", " ")), "[\\d]+", ""))
-        .withColumn('region_name_alt', when(col("region_name_tmp").isin(['Maputo City', 'Maputo Cidade']), 'Cidade de Maputo').otherwise(col("region_name_tmp")))
+        .withColumn('region_name_tmp', 
+                    regexp_replace(
+                        trim(
+                            regexp_replace(
+                                col("region_name"),
+                                 "[-\[\]–]+", " ")
+                        ),
+                        "[\\d]+", ""
+                    ))
+        .withColumn('region_name_alt',
+                     when(
+                         col("region_name_tmp").contains('Maputo City') | col("region_name_tmp").contains('Maputo Cidade'),
+                        'Cidade de Maputo'
+                     ).otherwise(
+                         when(
+                            col("region_name_tmp").contains('Maputo Province'),
+                            'Maputo'
+                         ).otherwise(col("region_name_tmp"))
+                     ))
         .drop('region_name_tmp')
         .join(countries, ["country_code"], "inner") # TODO: change to left & investigate dropped
     )
