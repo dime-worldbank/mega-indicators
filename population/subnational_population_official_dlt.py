bcaf9dc248324e66ca7b43a54725ef1407ce0450
weilu <weilu>
2023-11-17 21:45:17 +0000

    Construct normalized subnational population live table

---
+++ population/subnational_population_official_dlt.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+import dlt
+from pyspark.sql import functions as F
+
+# Adding a new country requires adding the country here
+@dlt.table(name=f'subnational_population')
+def subnational_population():
+    moz = spark.table('indicator_intermediate.moz_subnational_population')
+    pry = spark.table('boost_intermediate.pry_subnational_population')
+    return moz.union(pry)
