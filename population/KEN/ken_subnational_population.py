e4f8e5c55bea65d44c17e2cef281b127266581c6
bhupatiraju <bhupatiraju>
2023-11-29 14:01:00 +0000

    minor change by filtering adm1 regions from df_raw

--- population/KEN/ken_subnational_population.py
+++ population/KEN/ken_subnational_population.py
@@ -9,12 +9,12 @@ URL = 'https://www2.census.gov/programs-surveys/international-programs/tables/ti
 
 df_raw = pd.read_excel(URL, sheet_name='2000 - 2040', skiprows=2, header=None)
 # Read correct header
-df_raw.columns = header
 header = df_raw.iloc[1]
+df_raw.columns = header
 df_raw = df_raw.drop([0,1,2])
 
 # Extract Total population columns 
-df_pop_wide = df_raw[['CNTRY_NAME', 'ADM1_NAME']+[x for x in header if 'BTOTL' in x]]
+df_pop_wide = df_raw[df_raw.ADM_LEVEL==1][['CNTRY_NAME', 'ADM1_NAME']+[x for x in header if 'BTOTL' in x]]
 df_pop = pd.melt(df_pop_wide, id_vars=['CNTRY_NAME', 'ADM1_NAME'], var_name='year', value_name='population')
 df_pop['year'] = df_pop['year'].str.extract(r'(\d+)').astype(int)
 df_pop.columns = ['country_name', 'adm1_name', 'year', 'population']
@@ -30,7 +30,7 @@ df_pop = df_pop.sort_values(['adm1_name', 'year'], ignore_index=True)
 
 num_counties = df_pop.adm1_name.nunique()
 
-assert df_pop.shape[0] >= 1927, f'Expect at least 468 rows, got {df_pop.shape[0]}'
+assert df_pop.shape[0] >= 1927, f'Expect at least 1927 rows, got {df_pop.shape[0]}'
 assert all(df_pop.population.notnull()), f'Expect no missing values in population field, got {sum(df_pop.population.isnull())} null values'
 
 assert num_counties==47
