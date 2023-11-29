fae6eb46b8753acdc0b5897bed40879f96ca39ef
bhupatiraju <bhupatiraju>
2023-11-29 11:48:36 +0000

    Updated the method to merge multiple dataframes

--- population/subnational_population_official_dlt.py
+++ population/subnational_population_official_dlt.py
@@ -3,8 +3,13 @@ import dlt
 from pyspark.sql import functions as F
 
 # Adding a new country requires adding the country here
+country_codes = ['moz', 'pry', 'ken']
+
 @dlt.table(name=f'subnational_population')
 def subnational_population():
-    moz = spark.table('indicator_intermediate.moz_subnational_population')
-    pry = spark.table('indicator_intermediate.pry_subnational_population')
-    return moz.union(pry)
+    # Consolidating all the country specific dataframes
+    dfs = [spark.table(f'indicator_intermediate.{code}_subnational_population') for code in country_codes]
+    result_df = dfs[0]
+    for df in dfs[1:]:
+        result_df = result_df.unionByName(df)
+    return result_df
