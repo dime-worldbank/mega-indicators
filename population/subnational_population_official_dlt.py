9321af2043e213b0d2de9fac3ae9ae0754073792
bhupatiraju <bhupatiraju>
2024-01-12 14:15:10 +0000

    Added Democratic Republic of Congo (COD)

--- population/subnational_population_official_dlt.py
+++ population/subnational_population_official_dlt.py
@@ -3,7 +3,7 @@ import dlt
 from pyspark.sql import functions as F
 
 # Adding a new country requires adding the country here
-country_codes = ['moz', 'pry', 'ken', 'pak', 'bfa', 'col']
+country_codes = ['moz', 'pry', 'ken', 'pak', 'bfa', 'col', 'cod']
 
 @dlt.table(name=f'subnational_population')
 def subnational_population():
