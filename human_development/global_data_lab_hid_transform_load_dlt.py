61c2eecc5e65cce5c30626cc34b6ed8a0dad7d26
weilu <weilu>
2024-01-24 20:11:04 +0000

    Align Burkina Faso subnational region names in GDL HDI

--- human_development/global_data_lab_hid_transform_load_dlt.py
+++ human_development/global_data_lab_hid_transform_load_dlt.py
@@ -9,8 +9,15 @@ def hid_ed_index():
     ed = (spark.table(f'indicator_intermediate.global_data_lab_ed_index')
         .withColumnRenamed("ISO_Code", 'country_code')
         .join(countries, on=["country_code"], how="inner")
+        .withColumn("Region", F.trim(F.regexp_replace(F.col("Region"), "\\(.*\\)", "")))
         .withColumn("adm1_name", 
-            F.when(F.col("country_name") == 'Colombia',
+            F.when(F.col("country_name") == 'Burkina Faso',
+                F.when(F.col("Region") == 'Boucle de Mouhoun',
+                    F.lit("Boucle Du Mouhoun")
+                ).otherwise(
+                    F.regexp_replace(F.col("Region"), "-", " ")
+                )
+            ).when(F.col("country_name") == 'Colombia',
                 F.when(F.col("Region").contains('Valle'),
                     F.lit("Valle Del Cauca")
                 ).when(F.col("Region").contains('Bogota'),
@@ -26,7 +33,17 @@ def hid_ed_index():
                 ).when(F.col("Region") == 'Vaupis',
                     F.lit("Vaupes")
                 ).otherwise(
-                    F.trim(F.regexp_replace(F.col("Region"), "\\(.*\\)", ""))
+                    F.col("Region")
+                )
+            ).when(F.col("country_name") == 'Mozambique',
+                F.when(F.col("Region") == 'Maputo Cidade',
+                    F.lit("Cidade de Maputo")
+                ).when(F.col("Region") == 'Maputo Provincia',
+                    F.lit("Maputo")
+                ).when(F.col("Region") == 'Cabo delgado',
+                    F.lit("Cabo Delgado")
+                ).otherwise(
+                    F.col("Region")
                 )
             ).otherwise(
                 F.col("Region")
