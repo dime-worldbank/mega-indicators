7713e2e5a85a1be8c31dc1fdfbdf4aec123dca67
weilu <weilu>
2024-03-22 18:45:07 +0000

    Add per capita GDP PPP indicator

--- gdp.py
+++ gdp.py
@@ -9,21 +9,26 @@ import pandas as pd
 # COMMAND ----------
 
 wb.db = 2
-series = 'NY.GDP.MKTP.CN'
+indicators = ['NY.GDP.MKTP.CN', 'NY.GDP.PCAP.PP.KD']
+col_names = ['gdp_current_lcu', 'gdp_per_capita_2017_ppp']
 
 # COMMAND ----------
 
-df = wb.data.DataFrame(series, skipBlanks=True).reset_index()
-df
+long_dfs = []
+for series, col_name in zip(indicators, col_names):
+    df = wb.data.DataFrame(series, skipBlanks=True).reset_index()
+    long_df = df.melt(id_vars='economy', var_name='year', value_name=col_name)
+    long_df = long_df.dropna(subset=col_name)
+    long_df['year'] = long_df['year'].str.replace('YR', '')
+    long_df = long_df.astype({'year': 'int'})
+    long_dfs.append(long_df)
 
-# COMMAND ----------
+merged_df = long_dfs[0]
+for df in long_dfs[1:]:
+    merged_df = pd.merge(merged_df, df, on=['economy', 'year'])
 
-long_df = df.melt(id_vars='economy', var_name='year', value_name='gdp_current_lcu')
-long_df = long_df.dropna(subset='gdp_current_lcu')
-long_df['year'] = long_df['year'].str.replace('YR', '')
-long_df = long_df.astype({'year': 'int'})
-long_df['data_source'] = 'WB & OECD National Accounts'
-long_df
+merged_df['data_source'] = 'WB & OECD National Accounts'
+merged_df
 
 # COMMAND ----------
 
@@ -32,7 +37,7 @@ country_df
 
 # COMMAND ----------
 
-gdp_df = pd.merge(long_df, country_df, left_on='economy', right_on='country_code', how='left')[['country_name', 'country_code', 'region', 'year', 'gdp_current_lcu', 'data_source']]
+gdp_df = pd.merge(merged_df, country_df, left_on='economy', right_on='country_code', how='left')[['country_name', 'country_code', 'region', 'year', *col_names, 'data_source']]
 gdp_df
 
 # COMMAND ----------
