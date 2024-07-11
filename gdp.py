04dd18c773d32a0463c7b43ec25e2365f33ade58
weilu <weilu>
2024-07-11 02:59:36 +0000

    Refactor data fetch logic from wbgapi into reusable utils

--- gdp.py
+++ gdp.py
@@ -3,12 +3,10 @@
 
 # COMMAND ----------
 
-import wbgapi as wb
-import pandas as pd
+from utils import wbgapi_fetch
 
 # COMMAND ----------
 
-wb.db = 2
 indicators = [
     'NY.GDP.MKTP.CN',
     'NY.GDP.MKTP.CD',
@@ -26,35 +24,9 @@ col_names = [
     'gdp_per_capita_2017_ppp'
 ]
 
-# COMMAND ----------
+data_source = 'WB & OECD National Accounts, WB International Comparison Program, Eurostat-OECD PPP Programme'
 
-long_dfs = []
-for series, col_name in zip(indicators, col_names):
-    df = wb.data.DataFrame(series, skipBlanks=True).reset_index()
-    long_df = df.melt(id_vars='economy', var_name='year', value_name=col_name)
-    long_df = long_df.dropna(subset=col_name)
-    long_df['year'] = long_df['year'].str.replace('YR', '')
-    long_df = long_df.astype({'year': 'int'})
-    long_dfs.append(long_df)
+df = wbgapi_fetch(indicators, col_names, data_source)
 
-merged_df = long_dfs[0]
-for df in long_dfs[1:]:
-    merged_df = pd.merge(merged_df, df, on=['economy', 'year'])
-
-merged_df['data_source'] = 'WB & OECD National Accounts, WB International Comparison Program, Eurostat-OECD PPP Programme'
-merged_df
-
-# COMMAND ----------
-
-country_df = spark.table(f'indicator.country').select('country_name', 'country_code', 'region').toPandas()
-country_df
-
-# COMMAND ----------
-
-gdp_df = pd.merge(merged_df, country_df, left_on='economy', right_on='country_code', how='left')[['country_name', 'country_code', 'region', 'year', *col_names, 'data_source']]
-gdp_df
-
-# COMMAND ----------
-
-sdf = spark.createDataFrame(gdp_df)
+sdf = spark.createDataFrame(df)
 sdf.write.mode("overwrite").option("overwriteSchema", "true").saveAsTable("indicator.gdp")
