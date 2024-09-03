7c447f1067e48f6234b250c8950cbaa0d172b44b
yukinko-iwasaki <yukinko-iwasaki>
2024-09-03 21:38:21 +0000

    pull the data from API

--- energy/energy_generation_consumption.py
+++ energy/energy_generation_consumption.py
@@ -4,11 +4,36 @@ from pathlib import Path
 
 # COMMAND ----------
 
-# Bank firewall prenvents programmatic data fetch: https://ember-climate.org/data/api/
-# Curretnly requesting ITS to whitelist this request
+import requests
 
-file_path = 'https://raw.githubusercontent.com/weilu/mega-indicators/main/energy/2023-Country-Transition-Tracker-Consolidated-Format.csv'
-raw_df = pd.read_csv(file_path)
+my_api_key = dbutils.secrets.get(scope="DIMEBOOSTKEYVAULT", key="ember_energy_key")
+base_url = "https://api.ember-climate.org"
+query_url = (
+    f"{base_url}/v1/electricity-generation/yearly"
+    + f"?is_aggregate_series=false&api_key={my_api_key}"
+)
+
+response = requests.get(query_url)
+
+if response.status_code == 200:
+    data = response.json()
+
+# COMMAND ----------
+
+# Commented out the following code which loads the dataset from our static dataset
+# file_path ='https://raw.githubusercontent.com/weilu/mega-indicators/main/energy/2023-Country-Transition-Tracker-Consolidated-Format.csv'
+# raw_df = pd.read_csv(file_path)
+# raw_df = raw_df.loc[
+#     (raw_df["Category"] == "Electricity generation") & (raw_df["Unit"] == "%")
+# ]
+raw_df = pd.DataFrame(data["data"]).rename(
+    columns={
+        "date": "Year",
+        "entity_code": "Country code",
+        "series": "Variable",
+        "entity": "Area",
+    },
+)
 country_df = (
     spark.table(f"indicator.country")
     .select("country_name", "country_code", "region")
@@ -62,10 +87,6 @@ def categorize_fuel(row, type_dict):
 
 # COMMAND ----------
 
-energy_df = energy_df.loc[
-    (energy_df["Category"] == "Electricity generation") & (energy_df["Unit"] == "%")
-]
-
 energy_df = energy_df[energy_df["Variable"].isin(PRIMARY_TYPES)]
 energy_df["secondary_fuel_type"] = energy_df["Variable"].apply(
     categorize_fuel, type_dict=SECONDARY_TYPES
@@ -80,10 +101,13 @@ energy_df["quaternary_fuel_type"] = energy_df["Variable"].apply(
 # COMMAND ----------
 
 # Check for the countries whose share of the primary type fuel do not sum up to 100
-quality_check = (
-    energy_df.groupby(["country_name", "Year"]).sum("Variable").reset_index()
-)
-quality_check[(quality_check.Value < 99) | (quality_check.Value > 101)]
+quality_check = energy_df.groupby(["Area", "Year"]).sum("Variable").reset_index()
+
+# Note that Other Fossil dataset is missing, causing the quality check to fail for many countries
+quality_check[
+    (quality_check.share_of_generation_pct < 99)
+    | (quality_check.share_of_generation_pct > 101)
+]
 
 # COMMAND ----------
 
