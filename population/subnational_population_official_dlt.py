a8753f0c927cd24cedd98a8ede53b0af30d41acc
elysenko <elysenko>
2025-04-18 20:08:27 +0000

    adding liberia to subnational_population dlt

--- population/subnational_population_official_dlt.py
+++ population/subnational_population_official_dlt.py
@@ -3,7 +3,7 @@ import dlt
 from pyspark.sql import functions as F
 
 # Adding a new country requires adding the country here
-country_codes = ['moz', 'pry', 'ken', 'pak', 'bfa', 'col', 'cod', 'tun', 'btn', 'chl', 'nga', 'bgd', 'alb', "zaf", 'chl', 'gha']
+country_codes = ['moz', 'pry', 'ken', 'pak', 'bfa', 'col', 'cod', 'tun', 'btn', 'chl', 'nga', 'bgd', 'alb', "zaf", 'chl', 'gha', 'lbr']
 
 @dlt.table(name=f'subnational_population')
 def subnational_population():
