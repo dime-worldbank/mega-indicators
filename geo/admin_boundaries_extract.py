913ae5ab6aa59fdf5b06f80e32d5c5a879cdfff4
yukinko-iwasaki <yukinko-iwasaki>
2025-07-14 20:21:18 +0000

    remove internal error handling

--- geo/admin_boundaries_extract.py
+++ geo/admin_boundaries_extract.py
@@ -4,18 +4,13 @@ URL = 'https://datacatalogfiles.worldbank.org/ddh-published-v2/0038272/5/DR00953
 DATA_DIR = '/Volumes/prd_mega/sboost4/vboost4/Workspace/auxiliary_data/admin1geoboundaries'
 WB_ADM1_GEO_FILENAME = f'{DATA_DIR}/World Bank Official Boundaries - Admin 1.geojson'
 
-try:
-    # Send a GET request to the URL.
-    r = requests.get(URL)
-    r.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+# Send a GET request to the URL.
+r = requests.get(URL)
+r.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
 
-    # Open the local file in binary write mode
-    with open(WB_ADM1_GEO_FILENAME, 'wb') as f:
-        f.write(r.content) # Write the entire content at once
+# Open the local file in binary write mode
+with open(WB_ADM1_GEO_FILENAME, 'wb') as f:
+    f.write(r.content) # Write the entire content at once
 
-    print(f"File '{WB_ADM1_GEO_FILENAME}' downloaded successfully from '{URL}'")
-except requests.exceptions.RequestException as e:
-    print(f"Error downloading file: {e}")
-except IOError as e:
-    print(f"Error saving file to disk: {e}")
+print(f"File '{WB_ADM1_GEO_FILENAME}' downloaded successfully from '{URL}'")
 
