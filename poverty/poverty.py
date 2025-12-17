e034b8896d8f92bbcc3f0acef46db5b639f0a928
weilu <weilu>
2025-12-17 16:39:03 +0000

    Update national & subnat poverty source & data
    This is a breaking change as the poor* column names are changes to reflect the updated poverty lines

--- poverty/poverty.py
+++ poverty/poverty.py
@@ -5,18 +5,21 @@
 
 indicators = [
     'SI.POV.DDAY',
-    'SI.POV.LMIC.GP',
+    'SI.POV.LMIC',
     'SI.POV.UMIC',
 ]
 col_names = [
-    'poor215',
-    'poor365',
-    'poor685',
+    'poor300',
+    'poor420',
+    'poor830',
 ]
 
 data_source = 'WB Poverty and Inequality Platform'
 
 df = wbgapi_fetch(indicators, col_names, data_source)
+df
+
+# COMMAND ----------
 
 sdf = spark.createDataFrame(df)
 sdf.write.mode("overwrite").option("overwriteSchema", "true").saveAsTable("prd_mega.indicator.poverty")
