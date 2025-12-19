45ce3b879aa18ffe10710f512263c8d89f87f205
yukinko-iwasaki <yukinko.iwasaki@gmail.com>
2025-12-19 19:08:02 +0000

    population data fix

--- population/global_data_lab_subnational_population.r
+++ population/global_data_lab_subnational_population.r
@@ -29,46 +29,67 @@ set_countries_all <- function(sess) {
 
 # COMMAND ----------
 
+library(tidyr)
+library(readr)
+library(SparkR)
+sparkR.session(appName = "global_data_lab")
+
+# COMMAND ----------
+
 sess <- sess %>%
     set_dataset('demographics') %>%
     set_countries_all() %>%
-    set_indicators(c('regpopm'))
+    set_indicators(c('regpopm')) %>%
     # by default linear extrapolation for 3 years
     # disabling extrapolation doesn't seem to work
     # set_extrapolation_years_linear(0) %>%
-    # set_extrapolation_years_nearest(0) %>%
-    # set_interpolation(TRUE)
+    set_interpolation(TRUE)
 spop_merged <- gdl_request(sess)
+sdf <- createDataFrame(spop_merged)
+table_name <- paste0("prd_mega.indicator_intermediate.global_data_lab_subnational_population_bronze")
+saveAsTable(sdf, tableName = table_name, mode = "overwrite")
+
 print(colnames(spop_merged))
 print(paste('nrow:', nrow(spop_merged)))
 
 # COMMAND ----------
 
-library(tidyr)
-library(readr)
-library(SparkR)
-hive_config <- list("spark.sql.catalogImplementation" = "hive")
-sparkR.session(appName = "global_data_lab", config = hive_config)
+sdf <- SparkR::sql("SELECT * FROM prd_mega.indicator_intermediate.global_data_lab_subnational_population_bronze")
+df_population_bronze <-SparkR:: collect(sdf)
+
+# Rename columns in the R data.frame
+df_population_bronze <- df_population_bronze %>%
+  dplyr::rename(
+    year = Year,
+    population_millions = regpopm
+  )
+
+# Define the mapping vector for country name 
+renames <- c(
+  "Congo Democratic Republic" = "Congo, Dem. Rep.",
+  "Chili"                     = "Chile"
+)
+
+# Apply the replacements to the Country column
+df_population_bronze <- df_population_bronze %>%
+  dplyr::mutate(Country = dplyr::recode(Country, !!!renames))
 
 # COMMAND ----------
 
 library(dplyr)
 
-key <- 'X'
-
-df <- gather(spop_merged, key = key, value = 'population_millions', -Country, -Continent, -ISO_Code, -Level, -GDLCODE, -Region) %>%
-  mutate(year = as.integer(parse_number(key))) %>%
-  select(-key)
-
-df_no_extrapolation <- df %>%
+df_no_extrapolation <- df_population_bronze %>%
   filter(!is.na(population_millions)) %>%  # Drop rows where population_millions is null
   group_by(Country, Region) %>%
   arrange(Country, Region, desc(year)) %>%  
   filter(row_number() > 3, row_number() <= n() - 3) %>%  # Drop extrapolated years (first & last 3 years given Country, Region)
   ungroup()
 
+# COMMAND ----------
+
+library(dplyr)
 sdf <- createDataFrame(df_no_extrapolation)
-table_name <- paste0("prd_mega.indicator.global_data_lab_subnational_population")
+table_name <- paste0("prd_mega.indicator.global_data_lab_subnational_population_test")
 saveAsTable(sdf, tableName = table_name, mode = "overwrite")
 
 print(paste(table_name, 'nrow:', nrow(df_no_extrapolation)))
