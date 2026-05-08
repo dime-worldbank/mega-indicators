44aa0cef99dec688aa8da01f5cb4e4933634f045
Yukiko Suzuki <yukinko.iwasaki@gmail.com>
2026-05-08 10:05:58 -0400

    Update revenue.py

--- revenue.py
+++ revenue.py
@@ -4,13 +4,25 @@
 # COMMAND ----------
 
 indicators = [
-    'GC.REV.TOTL.GD'
+    'GC.REV.TOTL.GD',
+    'GC.TAX.TOTL.GD',
+    'GC.TAX.YPKG.RV.ZS',
+    'GC.TAX.INCD.RV.ZS',
+    'GC.TAX.CORP.RV.ZS',
+    'GC.TAX.GSRV.RV.ZS',
+    'GC.TAX.EXGS.RV.ZS'
 ]
 col_names = [
-    'total_revenue_pct_gdp'
+    'total_revenue_pct_gdp',
+    'tax_revenue_pct_gdp',
+    'tax_revenue_pct_gni',
+    'income_tax_pct_total_tax',
+    'corporate_income_tax_pct_total_tax',
+    'goods_services_tax_pct_total_tax',
+    'excise_tax_pct_total_tax'
 ]
 
-data_source = 'World Bank - General Government Revenue Statistics'
+data_source = 'World Bank - Government Revenue Statistics'
 
 df = wbgapi_fetch(indicators, col_names, data_source)
 
